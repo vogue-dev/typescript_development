30860d1f21e409c85cc428ca4e66b6fbb58da8ab
Mark Opria <oprya.mark@gmail.com>
2025-11-14 23:45:32 +0200

    homework to lesson 11

---
+++ src/exercise_11/src/server.ts
@@ -0,0 +1,30 @@
+// src/server.ts
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import taskRoutes from './routes/task.routes.js';
+import { errorHandler } from './controllers/task.controller.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// routes
+app.use('/tasks', taskRoutes);
+
+// 404
+app.use((req, res) => {
+    res.status(404).json({ error: 'Route not found' });
+});
+
+// errors
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
